bbb399802fd6ce09e196bdbd18450472f56eb583
Teddy <trosejr@terpmail.umd.edu>
2022-01-11 12:44:53 -0500

    Updated commit for user system

--- src/main/scala/RevCTP.scala
+++ src/main/scala/RevCTP.scala
@@ -3,16 +3,28 @@ import com.roundeights.hasher.Implicits._
 import scala.io.StdIn
 
 class RevCTP extends HiveDBC {
-  val SUCCESS = 0
-  val FAILURE = 1
+  val SUCCESS: Int = 0
+  val FAILURE: Int = 1
   val ERROR: Int = -1
   var employees: Map[String, (String, Long, String, String, Boolean)] = getEmployees(false)
   val init: Boolean = if(employees.size == 0) false else true
   var email: String = ""
   var loggedIn: Boolean = false
 
-  def initializeAdmin(): Unit = {
-    updateEmployeeInfo(false, true, true, true, true, false)
+  protected def initializeAdmin(): Unit = {
+    println("FIRST TIME USE> Please sign in with the default login to initialize your employee database.")
+    print("FIRST TIME USE> Enter your desired email -> ")
+    val email = StdIn.readLine()
+    print("FIRST TIME USE> Enter the desired password -> ")
+    val password = StdIn.readLine().sha256.hash
+    print("FIRST TIME USE> Enter your first name -> ")
+    val first_name = StdIn.readLine()
+    print("FIRST TIME USE> Enter your last name name -> ")
+    val last_name = StdIn.readLine()
+
+    createEmployee(employee_id = 8253, first_name, last_name, email, password, admin = true)
+    employees += email -> (password, 8253, first_name, last_name, true)
+    print("SYSTEM> Admin account successfully initialized!")
   }
   def login(): Int = {
     var tries: Int = 3
@@ -38,11 +50,27 @@ class RevCTP extends HiveDBC {
     }
     FAILURE
   }
-
-  override def createEmployee(employee_id: Long, first_name: String, last_name: String, email: String, password: String, admin: Boolean): Unit = {
-    super.createEmployee(employee_id, first_name, last_name, email, password, admin)
+  def logout(): Unit = {
+    if(loggedIn) {
+      email = ""
+      loggedIn = false
+      println(s"SYSTEM> User with email <$email> has been logged out.")
+    } else {
+      println("No user is logged in. Logout failed.")
+    }
   }
-
+  override def createEmployee(employee_id: Long, first_name: String, last_name: String, email: String, password: String, admin: Boolean): Unit = {
+    if(employees.contains(email)) {
+      println("SYSTEM> Email is already linked to an account.")
+    } else {
+      try {
+        super.createEmployee(employee_id, first_name, last_name, email, password, admin)
+        println(s"Account with email <$email> has been successfully created.")
+      } catch {
+        case _ => println(s"Account with email <$email> has not been created. Try again later.")
+      }
+    }
+  }
   def updateEmployeeInfo(terminate_bool: Boolean, first_name_bool: Boolean, last_name_bool: Boolean, email_bool: Boolean, password_bool: Boolean, admin_bool: Boolean): Unit = {
     val employee_id: Long = employees(email)._2
     var first_name: String = employees(email)._3
@@ -64,13 +92,16 @@ class RevCTP extends HiveDBC {
       }
       if (password_bool) {
         print("Enter the desired password-> ")
-        password = StdIn.readLine()
+        password = StdIn.readLine().sha256.hash
       }
+      employees += email -> (password, employee_id, first_name, last_name, admin_bool)
+    } else {
+      employees -= email
     }
     super.updateEmployeeInfo(terminate_bool, employee_id, first_name, last_name, email, password, admin_bool)
   }
   def checkEmail(e: String): Boolean = {
-    val emailRegex = """^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
+    val emailRegex = """^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])+(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$""".r
     e match {
       case null => false
       case e if e.trim.isEmpty => false
@@ -78,54 +109,51 @@ class RevCTP extends HiveDBC {
       case _ => false
     }
   }
-
 }
-object Main {
+object Main extends RevCTP {
   def main(args:Array[String]): Unit = {
     val rctp = new RevCTP()
     var currCommand = ""
-    println(s"SYSTEM> Welcome to the Revature Covid Tracker Planning app. We appreciate your new subscription to our service.")
-    //Initializes admin account
-    if (!rctp.init) {
-      println("FIRST TIME USE> Please sign in with the default login to initialize your employee database.")
-      print(s"FIRST TIME USE> Enter your desired email -> ")
-      val email = StdIn.readLine()
-      print(s"FIRST TIME USE> Enter the desired password -> ")
-      val password = StdIn.readLine().sha256.hash
-      print(s"FIRST TIME USE> Enter your first name -> ")
-      val first_name = StdIn.readLine()
-      print(s"FIRST TIME USE> Enter your last name name -> ")
-      val last_name = StdIn.readLine()
-      rctp.createEmployee(employee_id = 8253, first_name, last_name, email, password, admin = true)
-      print(s"SYSTEM> Admin account successfully made!")
+
+    println(s"SYSTEM> Welcome to the Revature Covid Tracker Planning app. We appreciate your subscription to our service.")
+
+    if (!rctp.init) { //Initializes admin account if there are no users in the database
+      initializeAdmin()
     }
 
     do {
       println("Enter in one of the following option numbers or type ':quit' to exit the program.")
+      println("Login/Logout")
       println("1. ")
       println("1. ")
       println("1. ")
       println("1. ")
-      println("1. ")
-      println("1. ")
-      println("1. ")
-
+      println("Settings")
+      println(":quit")
+      print("> ")
       currCommand = StdIn.readLine()
-      if(currCommand == "1") {
 
+      if(currCommand == "Login/Logout") {
+        if(loggedIn)
+          logout()
+        else
+          login()
       } else if(currCommand == "2") {
-
+        do {
+          print("> ")
+          val email = StdIn.readLine()
+          println(checkEmail(email))
+        } while(email != ":quit")
       } else if(currCommand == "3") {
 
-      } else if(currCommand == "4") {
-
-      } else if(currCommand == "5") {
-
-      } else if(currCommand == "6") {
+      } else if(currCommand == ":quit") {
+        println("Quitting application!")
+      } else if(currCommand == "Settings") { //Will be used to view and/or update user information
 
       } else {
-
+        println("SYSTEM> Invalid Choice!")
       }
     } while(currCommand != ":quit")
+
   }
 }
